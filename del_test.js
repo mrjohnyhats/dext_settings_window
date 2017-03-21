4d4db3a9786c6f639ccb5a7ced6d33fa4545f9fd
Period1 <FlexTime@AHS-137Lab-33.local>
2017-03-21 08:11:27 -0700

    removed del_test.js

--- del_test.js
+++
@@ -1,7 +0,0 @@
-const dutils = require('dext-core-utils');
-
-dutils.api.uninstall('dext-nova-theme', dutils.utils.paths.getPluginPath('dext-nova-theme')).then(() => {
-    console.log('works!')
-}, (err) => {
-    console.log('err: '+err);
-});
